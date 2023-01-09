b75f393f61e416472e3107ad64ae42d21d8433ba
J. Amaya <alejor016@gmail.com>
2023-01-09 13:22:54 +0100

    Update MobileInput.cs

--- Assets/Karting/Scripts/KartSystems/Inputs/MobileInput.cs
+++ Assets/Karting/Scripts/KartSystems/Inputs/MobileInput.cs
@@ -11,12 +11,31 @@ namespace KartGame.KartSystems
 
         public override InputData GenerateInput()
         {
+            bool breaking = IsBreaking();
             return new InputData
             {
-                Accelerate = Input.GetButton(AccelerateButtonName),
-                Brake = Input.GetButton(BrakeButtonName),
+                Accelerate = breaking ? false : IsAccelerating(),
+                Brake = breaking,
                 TurnInput = Input.GetAxis("Horizontal")
             };
+        }
+
+        bool IsBreaking()
+        {
+            foreach (Touch touch in Input.touches)
+            {
+                if (touch.position.x < Screen.width / 2) return true;
+            }
+            return false;
+        }
+
+        bool IsAccelerating()
+        {
+            foreach (Touch touch in Input.touches)
+            {
+                if (touch.position.x > Screen.width / 2) return true;
+            }
+            return false;
         }
     }
 }
